3d4985f05d74415ec75a491b6f12764466f0d86a
Fernando Jimenez <fgjv06@gmail.com>
2023-05-03 07:26:47 -0400

    JS - properties the player has

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ canvas.height = innerHeight;
 // class for the player
 class Player {
   // The constructor is called each time you instiate a new version of the player class, and we're gonna give it these individual properties to differentiate it from other players we might create
-  constructor() {
-
+  constructor(x, y, radius, color) {
+    // Properties the player has
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
   }
 }
