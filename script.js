8940bef027a1edd5fbec71347cbfb17e70cf9df6
Fernando Jimenez <fgjv06@gmail.com>
2023-05-03 07:23:51 -0400

    JS - The constructor is called each time you instiate a new version of the player class, and we're gonna give it these individual properties to differentiate it from other players we might create

--- script.js
+++ script.js
@@ -6,5 +6,8 @@ canvas.height = innerHeight;
 
 // class for the player
 class Player {
+  // The constructor is called each time you instiate a new version of the player class, and we're gonna give it these individual properties to differentiate it from other players we might create
+  constructor() {
 
+  }
 }
