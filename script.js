05f20ab323ce5abf54b64da1220ea863cee4833e
Fernando Jimenez <fgjv06@gmail.com>
2023-05-03 09:18:02 -0400

    JS - calling draw function from projectile

--- script.js
+++ script.js
@@ -32,6 +32,12 @@ class Projectile {
     this.color = color;
     this.velocity;
   }
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
 }
 
 const x = canvas.width / 2;
